6c0b505831f3c0b21e8a65459cdc6419e279bcd6
sergey-sergeev <sergey-sergeev.devops@yandex.ru>
2023-09-10 18:57:31 +0300

    merge: use shift - "теперь срипт будет отображать каждый переданный ему параметр отдельно"

--- branching/merge.sh
+++ branching/merge.sh
@@ -2,8 +2,8 @@
 # display command line options
 
 count=1
-for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+while [[ -n "$1" ]]; do
+    echo "Parameter #$count = $1"
     count=$(( $count + 1 ))
+    shift
 done
->>>>>>> c3d5de0 (merge: @ instead * "добавлен код в merge.sh")
