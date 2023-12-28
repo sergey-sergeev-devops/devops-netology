08c43e5ff98942c824eaadfcaf4514fa521dcde1
Sergey <sergeev-sergey-devops@yandex.ru>
2023-12-28 17:53:31 +0300

    fix 09-ci-03/example/fail.py

--- 09-ci-03/example/fail.py
+++ 09-ci-03/example/fail.py
@@ -1,5 +1,6 @@
+index = 0
 def increment(index):
-    index =+ 1
+    index += 1
     return index
 def get_square(numb):
     return numb*numb
@@ -7,7 +8,6 @@ def print_numb(numb):
     print("Number is {}".format(numb))
     pass
 
-index = 0
 while (index < 10):
     index = increment(index)
     print(get_square(index))
